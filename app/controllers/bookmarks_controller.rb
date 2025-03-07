64b831b0db3e25fa9b7cf65c3f7baa06e6cb29b4
Christian McCagherty <christianmccagherty164@gmail.com>
2025-03-07 10:50:01 +0000

    hotfix

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [ :create]
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
